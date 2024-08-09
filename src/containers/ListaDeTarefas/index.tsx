be15c7b621de16079e96d8843f82da6cb3de1ebb
EmersonPenelli <emersonpenelli@gmail.com>
2024-08-09 18:24:40 -0300

    styles: criado variaveis de cores e logica buttons

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,25 +1,44 @@
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
 
+const tarefas = [
+  {
+    titulo: 'Estudar Typescript',
+    descricao:
+      'Estudar Typescript para melhorar o desenvolvimento de aplicações React',
+    prioridade: 'Importante',
+    status: 'Em andamento'
+  },
+  {
+    titulo: 'Estudar Python',
+    descricao:
+      'Estudar Python para melhorar o desenvolvimento de aplicações Django',
+    prioridade: 'Importante',
+    status: 'Em andamento'
+  },
+  {
+    titulo: 'Estudar banco de dados',
+    descricao:
+      'Estudar banco de dados para melhorar a performance de consultas SQL',
+    prioridade: 'Importante',
+    status: 'Concluida'
+  }
+]
+
 const ListaDeTarefas = () => (
   <Container>
     <p>2 Tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            titulo={t.titulo}
+            status={t.status}
+            prioridade={t.prioridade}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
